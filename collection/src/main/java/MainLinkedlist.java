81dd003596267e6ddb2127bbbcbbcf04fb172b5c
praneelreddymallu <60325077+praneelreddymallu@users.noreply.github.com>
2020-03-06 06:40:14 -0800

    Add files via upload

---
+++ collection/src/main/java/MainLinkedlist.java
@@ -0,0 +1,15 @@
+
+public class MainLinkedlist {
+ 
+
+		public static void main(String args[])
+		{
+		Linkedlist a=new Linkedlist();
+		a.initiallist();
+		a.print();
+		a.selection();
+		}
+	}
+
+
+
